96b5d4d94ef637789802e5aa010e1e3e3ac54ab3
janaelazm <104913387+janaelazm@users.noreply.github.com>
2026-05-26 16:56:19 +0200

    assets for blocks and network logic

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 public class NetworkUI : MonoBehaviour
 {
+    [SerializeField] private string hostIP;
+    [SerializeField] private ushort port;
+
+    void Awake()
+    {
+        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.SetConnectionData(hostIP, port);
+    }
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
