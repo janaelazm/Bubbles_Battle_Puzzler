2cb1976a60c610f0a3d0406273e2523b48f47430
janaelazm <104913387+janaelazm@users.noreply.github.com>
2026-06-02 14:15:58 +0200

    add lobby code to UI

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -1,22 +1,39 @@
 using TMPro;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
+using System;
+using System.Threading.Tasks;
+using WebSocketSharp;
 
 public class NetworkUI : MonoBehaviour
 {
     [SerializeField] private TMP_InputField joinCodeInput;
+    [SerializeField] private TMP_Text roomCode;
 
-    public RelayManager relayManager;
+    [SerializeField] private RelayManager relayManager;
 
-    public void StartHost()
+    void Awake()
     {
-        relayManager.StartHost();
+        roomCode.gameObject.SetActive(false);
     }
 
-    public void StartClient()
+    public async void StartHost()
     {
+        roomCode.text = "Generating Code...";
+        roomCode.gameObject.SetActive(true);
+        await relayManager.StartHost();
+        roomCode.text = "Room Code: " + relayManager.joinCode;
+    }
+
+    public async void StartClient()
+    {
+        roomCode.text = "Joining Room...";
         string code = joinCodeInput.text;
-        Debug.Log("joinging game:" + code);
-        relayManager.StartClient(code);
+        if(code.IsNullOrEmpty())
+            return;
+        await relayManager.StartClient(code);
+        roomCode.gameObject.SetActive(true);
+        roomCode.text = "Room Code: " + code;
     }
 }
--- Assets/Scripts/RelayManager.cs
+++ Assets/Scripts/RelayManager.cs
@@ -4,19 +4,20 @@ using Unity.Services.Relay.Models;
 using Unity.Netcode.Transports.UTP;
 using Unity.Netcode;
 using System;
+using System.Threading.Tasks;
 public class RelayManager : MonoBehaviour
 {
-    public async void StartHost()
+    public string joinCode;
+    public async Task StartHost()
     {
         try
         {
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(4);
 
-            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
             Debug.Log("JOIN CODE: " + joinCode);
-            var transport =
-            NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+            var transport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
 
             transport.SetHostRelayData(
                 allocation.RelayServer.IpV4,
@@ -36,7 +37,7 @@ public class RelayManager : MonoBehaviour
         }
     }
 
-    public async void StartClient(string joinCode)
+    public async Task StartClient(string joinCode)
     {
         JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode.Trim());
 
