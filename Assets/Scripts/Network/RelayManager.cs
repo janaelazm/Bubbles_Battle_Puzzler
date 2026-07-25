535557908d298de480a08890dea0049052eb3719
janaelazm <104913387+janaelazm@users.noreply.github.com>
2026-07-25 14:05:05 +0200

    add lobby manager and quickplay

--- Assets/Scripts/Network/RelayManager.cs
+++ Assets/Scripts/Network/RelayManager.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
-using Unity.Netcode.Transports.UTP;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using System;
 using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
 public class RelayManager : MonoBehaviour
 {
+    public event Action<string> OnStatusChanged;
+    public event Action<string> OnRoomCodeGenerated;
+
+
     public async Task<string> StartHost()
     {
         try
         {
+            OnStatusChanged?.Invoke("Generating room...");
+
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(4);
 
             string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
             Debug.Log("JOIN CODE: " + joinCode);
-            var transport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
 
             transport.SetHostRelayData(
                 allocation.RelayServer.IpV4,
@@ -28,19 +37,40 @@ public class RelayManager : MonoBehaviour
 
             NetworkManager.Singleton.StartHost();
 
-            Debug.Log("Host started");
+            OnRoomCodeGenerated?.Invoke(joinCode);
+            OnStatusChanged?.Invoke("Waiting for player to join...");
+
+
+            NetworkManager.Singleton.OnClientConnectedCallback += clientId =>
+            {
+                Debug.Log("Client connected: " + clientId);
+
+                OnStatusChanged?.Invoke("Player joined!");
+
+                NetworkManager.Singleton.SceneManager.LoadScene(
+                    "PathSelection",
+                    LoadSceneMode.Single
+                );
+            };
+
+
             return joinCode;
         }
         catch (Exception e)
         {
             Debug.LogException(e);
+            OnStatusChanged?.Invoke("Failed to create room.");
             throw;
         }
     }
 
+
     public async Task StartClient(string joinCode)
     {
-        try{
+        try
+        {
+            OnStatusChanged?.Invoke("Joining room...");
+
             JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode.Trim());
 
             var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
@@ -55,10 +85,13 @@ public class RelayManager : MonoBehaviour
             );
 
             NetworkManager.Singleton.StartClient();
+
+            OnStatusChanged?.Invoke("Connected!");
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Debug.LogException(e);
+            OnStatusChanged?.Invoke("Failed to join room.");
             throw;
         }
     }
